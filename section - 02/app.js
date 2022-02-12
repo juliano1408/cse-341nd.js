647e68e6b925b3048bee1d68fb5c2ed6806324f4
juliano1408 <bbfurlong@gmail.com>
2022-02-12 13:55:43 -0500

    Code udpated

---
+++ section - 02/app.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser'); //npm install --save body-parser
+const expressHbs = require('express-handlebars');
+
+const app = express(); 
+
+// For Handlebars
+// app.engine(
+//     'hbs', 
+//     expressHbs({
+//         layoutsDir: 'views/layouts/', 
+//         defaultLayout: 'main-layout',
+//         extname: 'hbs'
+//     })
+// );
+
+app.set('view engine', 'ejs');
+app.set('views', 'views'); // file path to template
+
+const rootDir = require('./util/path.js');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
+app.listen(5005);
